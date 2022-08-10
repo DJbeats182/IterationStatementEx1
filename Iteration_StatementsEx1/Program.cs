4b23e698345e8b7ff4be2027373cc7baf49c154b
DJ Bates <blooviper@gmail.com>
2022-08-10 14:00:07 -0600

    First Commit

---
+++ Iteration_StatementsEx1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Iteration_StatementsEx1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i < 16; i++)
+                Console.WriteLine(i);
+        }
+    }
+}
